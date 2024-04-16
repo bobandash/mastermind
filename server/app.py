c3bec8eeea89870a6d6101c90fc38f4e929ce945
Bruce <bruce@personaify.ai>
2024-04-16 12:56:51 -0700

    Add basic models needed for single player functionality

--- server/app.py
+++ server/app.py
@@ -4,6 +4,11 @@ from models import db
 from flask_migrate import Migrate
 import os
 
+# TODO: for the mvp, will implement with JWT Token to save time
+# however, for the game routes from my understanding, there's not a lot of performance benefit.
+# the statelessness of jwt token for the majority of the routes is not needed, you would have the make db calls with the user_id
+# If I use session based auth with redis as cache layer, I can at least logout the users if they get hacked
+
 
 def create_app():
     load_dotenv()
