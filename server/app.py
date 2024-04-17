aa7efc65a6f699d1a5bee93757dce0a192df4dbb
Bruce <bruce@personaify.ai>
2024-04-17 13:10:06 -0700

    Add server-session based auth

--- server/app.py
+++ server/app.py
@@ -2,9 +2,9 @@ from flask import Flask
 from dotenv import load_dotenv
 from models.models import db
 from flask_migrate import Migrate
+from flask_session import Session
 from routes import auth_bp, game_bp, round_bp, user_bp
-
-# TODO: for the mvp, will implement with JWT Token to save time; look into Redis and session tokens (not huge priority)
+from routes.auth_bp import bcrypt
 
 
 def create_app():
@@ -12,6 +12,8 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object("config.Config")
     db.init_app(app)
+    bcrypt.init_app(app)
+    server_session = Session(app)
     migrate = Migrate(app, db)
     app.register_blueprint(auth_bp.auth_bp, url_prefix="/auth")
     app.register_blueprint(user_bp.user_bp, url_prefix="/users")
--- server/config.py
+++ server/config.py
@@ -1,7 +1,14 @@
 import os
+import redis
 
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL", "postgresql://username:password@localhost/database_name"
     )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY", "secret_key")
+    SESSION_TYPE = "redis"
+    SESSION_PERMANENT = False
+    SESSION_USE_SIGNER = True
+    SESSION_REDIS = redis.from_url(os.getenv("REDIS_URL", "127.0.0.1:6379"))
