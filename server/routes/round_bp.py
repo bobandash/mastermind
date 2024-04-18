6449ff1be2a242f26f6fd8b501b4e4c6227235d4
Bruce <bruce@personaify.ai>
2024-04-18 16:17:11 -0700

    Refactor: common game logic to utils folder

--- server/routes/round_bp.py
+++ server/routes/round_bp.py
@@ -4,53 +4,10 @@ from models.models import Turn, db
 from flask import session, jsonify, request
 from util.decorators import session_required, check_user_is_codebreaker
 from util.enum import StatusEnum
+from util.game_logic import is_guess_proper_format, calculate_result
 import json
 
 round_bp = Blueprint("round_bp", __name__)
-
-
-def is_guess_proper_format(guess, secret_code, num_colors):
-    if len(secret_code) != len(guess):
-        return False
-    if not all([isinstance(g, int) for g in guess]):
-        return False
-    if max(guess) >= num_colors or min(guess) < 0:
-        return False
-    return True
-
-
-def calculate_result(guess, secret_code):
-    black_pegs = 0
-    white_pegs = 0
-    remaining_pegs_guess = {}
-    remaining_pegs_secret_code = {}
-    for i in range(len(guess)):
-        if guess[i] == secret_code[i]:
-            black_pegs += 1
-        else:
-            remaining_pegs_guess[guess[i]] = remaining_pegs_guess.get(guess[i], 0) + 1
-            remaining_pegs_secret_code[secret_code[i]] = (
-                remaining_pegs_secret_code.get(secret_code[i], 0) + 1
-            )
-
-    for key in remaining_pegs_guess.keys():
-        if key in remaining_pegs_secret_code:
-            white_pegs += min(
-                remaining_pegs_guess[key], remaining_pegs_secret_code[key]
-            )
-    won_round = black_pegs == len(guess)
-    correct_numbers = white_pegs + black_pegs
-    is_plural = correct_numbers > 1
-    if not white_pegs and not black_pegs:
-        message = "All incorrect."
-    else:
-        message = f"{correct_numbers} correct number{'s' if is_plural else ''} and {black_pegs} correct location"
-    return {
-        "won_round": won_round,
-        "black_pegs": black_pegs,
-        "white_pegs": white_pegs,
-        "message": message,
-    }
 
 
 @round_bp.route("/<round_id>/turns", methods=["POST"])
